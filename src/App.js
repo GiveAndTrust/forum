6ffeed2023500eb6d6196c328ae2f1a366430bb0
Dimitri Dekker <dimtri_dekker@hotmail.com>
2018-06-03 10:28:15 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <h2>Hallo ik ben Dimitri</h2>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button>press me!
+          </button>
+      </div>
+    );
+  }
+}
+
+export default App;
